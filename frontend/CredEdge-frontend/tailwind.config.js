6dc82a81af0cd8872f0cd78ed3021e02fa25863f
kapilkaranmathur <kapilkaranmathur@gmail.com>
2026-01-15 02:10:22 +0530

    first commit

---
+++ frontend/CredEdge-frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'auth-bg-img': "url('/src/assets/images/image1.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
