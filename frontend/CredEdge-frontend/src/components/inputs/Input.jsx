0b943e194c0e8cb0d23cf7edcd08902f3deab393
kapilkaranmathur <kapilkaranmathur@gmail.com>
2026-02-15 03:26:03 +0530

    reusable input and profile selector

---
+++ frontend/CredEdge-frontend/src/components/inputs/Input.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
+
+const Input = ({ value, onChange, placeholder, label, type }) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  return (
+    <div>
+      <label className="text-13px text-slate-800 dark:text-gray-300">
+        {label}
+      </label>
+
+      <div className="input-box">
+        <input
+          type={
+            type == "password" ? (showPassword ? "text" : "password") : type
+          }
+          placeholder={placeholder}
+          className="w-full bg-transparent outline-none"
+          value={value}
+          onChange={(i) => onChange(i)}
+        />
+
+        {type === "password" && (
+          <>
+            {showPassword ? (
+              <FaRegEye
+                size={22}
+                className="cursor-pointer text-primary"
+                onClick={toggleShowPassword}
+              />
+            ) : (
+              <FaRegEyeSlash
+                size={22}
+                className="text-slate-400 cursor-pointer"
+                onClick={toggleShowPassword}
+              />
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Input;
